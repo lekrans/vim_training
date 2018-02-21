1f6a29b8abc60121a99be8b0bf82da3dfe8de529
Michael Lekrans <mlekrans@hotmail.com>
2018-02-21 05:56:33 +0100

    added tip 5

---
+++ src/questions/0_The_vim_way/1_vim/tip_5_find_and_replace_by_hand.js
@@ -0,0 +1,27 @@
+`
+  KEY:  * /
+
+
+  we want to change content with copy.. but not in the second line
+
+  start on beginning of line 1
+
+  /content
+  cw copy <Esc>
+  n
+  n
+  .
+`
+
+
+...We're waiting for content before the site can go live...
+...If you are content with this, let's go ahead with it...
+...We'll launch as soon as we have the content...
+
+`
+  result
+...We're waiting for copy before the site can go live...
+...If you are content with this, let's go ahead with it...
+...We'll launch as soon as we have the copy...
+
+`
